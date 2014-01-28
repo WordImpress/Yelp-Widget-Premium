2b305cc05e44ac9e4b3e6563a8e973f21020e63b
Devin Walker <devin@imdev.in>
2014-01-28 12:48:57 -0800

    Bumped up version number

--- yelp-widget-pro.php
+++ yelp-widget-pro.php
@@ -3,7 +3,7 @@
 Plugin Name: Yelp Widget Pro Premium
 Plugin URI: http://wordimpress.com/wordpress-plugin-development/yelp-widget-pro/
 Description: Easily display Yelp business ratings with a simple and intuitive WordPress widget.
-Version: 1.8
+Version: 1.8.1
 Author: Devin Walker
 Author URI: http://imdev.in/
 License: GPLv2
